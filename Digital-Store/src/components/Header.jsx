b52dd54b7813db0994f9002f4e98156ef6e7b4e1
breno-oliveira98 <breno.oliveira2011@hotmail.com>
2024-11-19 14:45:25 -0300

    Criando componente Logo

--- Digital-Store/src/components/Header.jsx
+++ Digital-Store/src/components/Header.jsx
@@ -2,18 +2,21 @@ import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import iconLogo from "../assets/vector.png";
 import search from "../assets/Search.png";
-import buy from "../assets/Buy.png"
+import buy from "../assets/Buy.png";
 
 const Header = () => {
   return (
-    <div>
+    <header>
       <div className="w-full h-[190px] flex justify-center py-6  gap-8">
         <div className="w-[70%]">
           <div className="flex justify-center items-center gap-8 px-4">
+            {/* Logo Section */}
             <div className="flex items-center gap-1">
-              <img src={iconLogo} alt="Icone Logo Marca" />
-              <img className="mt-1.5" src={logo} alt="Logo Marca" />
+              <img src={iconLogo} alt="Ícone da Marca" />
+              <img className="mt-1.5" src={logo} alt="Logo da Marca" />
             </div>
+
+            {/* Search Input */}
             <div className="relative flex-1">
               <input
                 type="text"
@@ -25,33 +28,48 @@ const Header = () => {
                 alt="Search"
                 className="absolute right-4 cursor-pointer top-1/2 transform -translate-y-1/2 w-5"
               />
+              
             </div>
-
+            {/* Register Link */}
             <Link className="underline">Cadastre-se</Link>
           </div>
-          <ul className="flex gap-8 ml-4 mt-16">
-            <li>
-              <Link className="font-normal text-[var(--dark-gray)] hover:text-[var(--primary)] hover:font-semibold hover:underline ">Home</Link>
-            </li>
-            <li>
-              <Link className="font-normal text-[var(--dark-gray)] hover:text-[var(--primary)] hover:font-semibold hover:underline ">Produtos</Link>
-            </li>
-            <li>
-              <Link className="font-normal text-[var(--dark-gray)] hover:text-[var(--primary)] hover:font-semibold hover:underline ">Categorias</Link>
-            </li>
-            <li>
-              <Link className="font-normal text-[var(--dark-gray)] hover:text-[var(--primary)] hover:font-semibold hover:underline ">Meus Pedidos</Link>
-            </li>
-          </ul>
+
+          {/* Navigations Links */}
+          <nav>
+            <ul className="flex gap-8 ml-4 mt-16">
+              <li>
+                <Link className="font-normal text-[var(--dark-gray)] hover:text-[var(--primary)] hover:font-semibold hover:underline ">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link className="font-normal text-[var(--dark-gray)] hover:text-[var(--primary)] hover:font-semibold hover:underline ">
+                  Produtos
+                </Link>
+              </li>
+              <li>
+                <Link className="font-normal text-[var(--dark-gray)] hover:text-[var(--primary)] hover:font-semibold hover:underline ">
+                  Categorias
+                </Link>
+              </li>
+              <li>
+                <Link className="font-normal text-[var(--dark-gray)] hover:text-[var(--primary)] hover:font-semibold hover:underline ">
+                  Meus Pedidos
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
+
+        {/* Login Button and Cart Icon */}
         <button className="w-36 h-10 mt-2 text-white rounded-lg bg-[var(--primary)]">
           Entrar
         </button>
         <span className="mt-4">
-            <img src={buy} alt="" />
+          <img src={buy} alt="Ícone de Carrinho de Compras" />
         </span>
       </div>
-    </div>
+    </header>
   );
 };
 
---
+++ Digital-Store/src/components/Logo.jsx
@@ -0,0 +1,8 @@
+import logoMarca from "../assets/logo.png"
+export const Logo = () => {
+    return ( 
+        <>
+            <img className="w-[253px] h-[44px]" src={logoMarca} alt="Logo da Marca" />
+        </>
+     );
+}
