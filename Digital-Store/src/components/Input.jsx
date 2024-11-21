66bba26bbcdf0bd41373c56ecedfca8053afcca3
breno-oliveira98 <breno.oliveira2011@hotmail.com>
2024-11-21 11:33:57 -0300

    cor e tamanho da label do input

--- Digital-Store/src/components/Input.jsx
+++ Digital-Store/src/components/Input.jsx
@@ -1,7 +1,7 @@
 export const Input = ({id, label, type = "text", placeholder, onChange}) => {
     return ( 
         <>
-            <label htmlFor={id}>{label}</label>
+            <label htmlFor={id} className="text-xs text-[var(--dark-gray-2)]">{label}</label>
             <input className="outline-none w-full h-10 p-3 bg-[var(--light-gray-3)]" type={type} id={id} placeholder={placeholder} onChange={onChange}/>
         </>
      );
