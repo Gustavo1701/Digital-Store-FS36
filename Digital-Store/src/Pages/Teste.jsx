b5bf646d1fae8fadfbde10a313ef308e3d1ee846
Gustavo1701 <gustavojr.lg.10@gmail.com>
2024-11-23 12:52:17 -0300

    Testando criação do componente Collection-Destaque

--- Digital-Store/src/Pages/Teste.jsx
+++ Digital-Store/src/Pages/Teste.jsx
@@ -1,14 +1,13 @@
+import Destaque from "../components/Collection-Destaque";
 import { Input } from "../components/Input";
 
 export const Teste = () => {
     return ( 
         <>
             <h1>Teste</h1>
-            <Input
-            label={"Joao"}
             
-        
-            />
+            <Destaque/>
+            
         </>
      );
 }
