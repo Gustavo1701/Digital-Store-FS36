96cdefe6e3e225c1e5869f960f6858738124661c
Gustavo1701 <gustavojr.lg.10@gmail.com>
2024-11-23 12:42:50 -0300

    Teste

--- Digital-Store/src/Pages/HomePage.jsx
+++ Digital-Store/src/Pages/HomePage.jsx
@@ -4,22 +4,22 @@ import { Section } from "../components/Section";
 const HomePage = () => {
     return (
         <>
-        <Section
-         title={"Produtos em Alta"}
-         
-         
-        />
+            <Section
+                title={"Produtos em Alta"}
 
-        <Gallery 
-             slides={[
-                '../public/home-slide-1.jpeg',
-                '../public/home-slide-2.jpeg',
-                '../public/home-slide-3.jpeg',
-                '../public/home-slide-5.jpeg',
-                '../public/home-slide-6.jpeg',
-                '../public/home-slide-7.jpeg',
-                '../public/home-slide-8.jpeg',
-              ]}
+
+            />
+
+            <Gallery
+                slides={[
+                    '../public/home-slide-1.jpeg',
+                    '../public/home-slide-2.jpeg',
+                    '../public/home-slide-3.jpeg',
+                    '../public/home-slide-5.jpeg',
+                    '../public/home-slide-6.jpeg',
+                    '../public/home-slide-7.jpeg',
+                    '../public/home-slide-8.jpeg',
+                ]}
             />
         </>
     );
