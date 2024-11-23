f6c03cf711468f69a16eeabcbd4e90d72f7ec061
Breno Oliveira <breno.oliveira2011@hotmail.com>
2024-11-23 13:50:06 -0300

    product card

---
+++ Digital-Store/src/components/ProductCard.jsx
@@ -0,0 +1,8 @@
+import product from "../../public/product-image-5.jpeg"
+export const ProductCard = () => {
+    return ( 
+        <div className="w-[292px] h-[439px]">
+            <img src={product} alt="" />
+        </div>
+     );
+}
