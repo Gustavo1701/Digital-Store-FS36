d046c4cc790cfe18a5fed50ace824d0e21076fb1
João Roberto de Oliveira Felix <joao.robertodof@gmail.com>
2024-11-23 12:27:48 -0300

    Adicionando funcionalidade carrosel e todas as imagens de public

--- Digital-Store/src/Pages/HomePage.jsx
+++ Digital-Store/src/Pages/HomePage.jsx
@@ -1,4 +1,4 @@
-
+import Gallery from "../components/Gallery";
 import { Section } from "../components/Section";
 
 const HomePage = () => {
@@ -7,8 +7,20 @@ const HomePage = () => {
         <Section
          title={"Produtos em Alta"}
          
-
+         
         />
+
+        <Gallery 
+             slides={[
+                '../public/home-slide-1.jpeg',
+                '../public/home-slide-2.jpeg',
+                '../public/home-slide-3.jpeg',
+                '../public/home-slide-5.jpeg',
+                '../public/home-slide-6.jpeg',
+                '../public/home-slide-7.jpeg',
+                '../public/home-slide-8.jpeg',
+              ]}
+            />
         </>
      );
 }
